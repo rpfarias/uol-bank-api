eecb08c886481744dee76c3f17ccffa54bb6a7b6
Rafael Farias <rpf1rafael@gmail.com>
2026-06-07 16:15:05 -0300

    Initial commit

---
+++ src/main/java/com/rafaelfarias/uol_bank_api/UolBankApiApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelfarias.uol_bank_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UolBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UolBankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelfarias/uol_bank_api/UolBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelfarias.uol_bank_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UolBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
